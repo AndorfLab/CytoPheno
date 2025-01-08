9f455434e65c7e5cba70018e586668193981e96e
AmandaRT18 <amanda.tursi@gmail.com>
2025-01-08 16:46:30 -0500

    Update example-files.R

--- R/example-files.R
+++ R/example-files.R
@@ -1,17 +1,15 @@
 # Example files
 
-Kimmey_5hr_stim2 <- read.csv("./data/Kimmey_5hr_stim.csv", header = TRUE)
+#Kimmey_5hr_stim2 <- read.csv("./data/Kimmey_5hr_stim.csv", header = TRUE)
 
-Dusoswa_OMIP_54_markers2 <- read.csv("./data/Dusoswa_OMIP_54_markers.csv", header = TRUE)
+#Dusoswa_OMIP_54_markers2 <- read.csv("./data/Dusoswa_OMIP_54_markers.csv", header = TRUE)
 
-Lee_AML_cell_types_markers2 <- read.csv("./data/Lee_AML_cell_types_markers.csv", header = TRUE)
+#Lee_AML_cell_types_markers2 <- read.csv("./data/Lee_AML_cell_types_markers.csv", header = TRUE)
 
+Kimmey_5hr_stim2 <- read.csv("Kimmey_5hr_stim.csv", header = TRUE)
 
-# Kimmey <- load(Kimmey_5hr_stim)
-#
-# Kimmey_5hr_stim2 <- read.csv("./data/Kimmey_5hr_stim.rda", header = TRUE)
-#
-# #Dusoswa_OMIP_54_markers2 <- Dusoswa_OMIP_54_markers
-#
-# #Lee_AML_cell_types_markers2 <- Lee_AML_cell_types_markers
+Dusoswa_OMIP_54_markers2 <- read.csv("Dusoswa_OMIP_54_markers.csv", header = TRUE)
+
+Lee_AML_cell_types_markers2 <- read.csv("Lee_AML_cell_types_markers.csv", header = TRUE)
+
 
