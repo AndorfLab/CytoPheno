1dcb8bedae1a0e092372a01f2c1213e76d30c25b
AmandaRT18 <amanda.tursi@gmail.com>
2025-01-08 16:41:17 -0500

    Update server.R

--- R/server.R
+++ R/server.R
@@ -32,14 +32,15 @@
 #'
 #' @return server
 #'
-source("./R/endpoints.R")
-source("./R/protein_SPARQL.R")
-source("./R/integer_breaks.R")
-source("./R/example-files.R")
+#source("./R/endpoints.R")
+#source("./R/protein_SPARQL.R")
+#source("./R/integer_breaks.R")
+#source("./R/example-files.R")
 
-#source("endpoints.R")
-#source("protein_SPARQL.R")
-#source("integer_breaks.R")
+source("endpoints.R")
+source("protein_SPARQL.R")
+source("integer_breaks.R")
+source("example-files.R")
 
 # Increase max size for uploaded files
 options(shiny.maxRequestSize = 60*1024^2)
