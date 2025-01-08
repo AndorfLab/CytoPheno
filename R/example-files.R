238e6de6d5e7b8f268f48465b8e798dc0246d46a
AmandaRT18 <amanda.tursi@gmail.com>
2025-01-08 16:49:09 -0500

    Update example-files.R

--- R/example-files.R
+++ R/example-files.R
@@ -6,10 +6,10 @@
 
 #Lee_AML_cell_types_markers2 <- read.csv("./data/Lee_AML_cell_types_markers.csv", header = TRUE)
 
-Kimmey_5hr_stim2 <- read.csv("Kimmey_5hr_stim.csv", header = TRUE)
+Kimmey_5hr_stim2 <- read.csv("./Kimmey_5hr_stim.csv", header = TRUE)
 
-Dusoswa_OMIP_54_markers2 <- read.csv("Dusoswa_OMIP_54_markers.csv", header = TRUE)
+Dusoswa_OMIP_54_markers2 <- read.csv("./Dusoswa_OMIP_54_markers.csv", header = TRUE)
 
-Lee_AML_cell_types_markers2 <- read.csv("Lee_AML_cell_types_markers.csv", header = TRUE)
+Lee_AML_cell_types_markers2 <- read.csv("./Lee_AML_cell_types_markers.csv", header = TRUE)
 
 
