e955caff65f589e5ee72771092061a1bca32ced3
AmandaRT18 <amanda.tursi@gmail.com>
2025-01-13 12:24:56 -0500

    Update app.R

--- R/app.R
+++ R/app.R
@@ -1,4 +1,18 @@
 # Run Shiny app
+#
+# Cell.Naming: Intakes post-clustered cytometry data and denotes marker description patterns and descriptive cell type names.
+# Copyright (C) 2025 Amanda Tursi
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
 
 library(shiny)
 source("./R/ui.R")
